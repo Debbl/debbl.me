ef4251f0e5b81eaf572b3270dc6813dd418af3d0
Debbl <me@aiwan.run>
2022-06-11 22:40:36 +0800

    Update02

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -9,5 +9,5 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  return new Response(data);
+  return new Response(JSON.stringify(context));
 }
